1ec2ec6b74f5909d6d8a4e8ca9a0ba08baecdb7a
ChamplainBrent <90007085+ChamplainBrent@users.noreply.github.com>
2023-04-11 14:35:02 -0400

    Update Example.py

--- Example.py
+++ Example.py
@@ -1,26 +1,12 @@
 import altair as alt
-from vega_datasets import data
-import streamlit as st
+import pandas as pd
 
-source = data.seattle_weather()
-
-brush = alt.selection_interval(encodings=['x'])
-
-bars = alt.Chart().mark_bar().encode(
-    x='month(date):O',
-    y='mean(precipitation):Q',
-    opacity=alt.condition(brush, alt.OpacityValue(1), alt.OpacityValue(0.3)),
-).add_params(
-    brush
+source = pd.DataFrame.from_records(
+    [{'a': 1, 'b': 1, 'image': 'https://altair-viz.github.io/_static/altair-logo-light.png'},
+     {'a': 2, 'b': 2, 'image': 'https://avatars.githubusercontent.com/u/11796929?s=200&v=4'}]
 )
-
-line = alt.Chart().mark_rule(color='firebrick').encode(
-    y='mean(precipitation):Q',
-    size=alt.SizeValue(3)
-).transform_filter(
-    brush
+alt.Chart(source).mark_circle(size=200).encode(
+    x='a',
+    y='b',
+    tooltip=['image']  # Must be a list containing a field called "image"
 )
-
-c = alt.layer(bars, line, data=source)
-
-st.altair_chart(c, use_container_width=True)
